1705556f4816f7c6f0880fc971366b6fa12db4d5
mukeshsaini143 <94359081+mukeshsaini143@users.noreply.github.com>
2024-02-08 22:37:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 
 provider "aws" {
   region = "us-east-1"  # Set your desired region
-  access_key = "AKIA46A62KETIMG45G6E"
-  secret_key = "DAgJA3bh/Brk6IGIamhWSBocekOsxIbWqt5Nagn0"
+  #access_key = "AKIA46A62KETIMG45G6E"
+  #secret_key = "DAgJA3bh/Brk6IGIamhWSBocekOsxIbWqt5Nagn0"
   assume_role {
-    role_arn = "arn:aws:iam::889122869542:role/ec2-full-access"
+    role_arn = "arn:aws:iam::889122869542:role/ec2-full-access-12"
   }
 }
 
